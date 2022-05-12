7c8236688e6a51078cdd2ac3568871378bc430fe
zhuqingsong <735560190@qq.com>
2022-05-12 10:56:53 +0800

    elementui

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import App from '@/view/App'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import router from '@/router/index'
+import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui'
 
 Vue.use(VueRouter)
+Vue.use(ElementUI)
 
 new Vue({
     el: '#app',
